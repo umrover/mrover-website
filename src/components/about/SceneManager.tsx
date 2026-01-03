f3d6d6ea62033a49c8b49216aa54178923dfcf68
kevinjin420 <kevinjin0420@gmail.com>
2026-01-02 18:24:37 -0700

    e

--- src/components/about/SceneManager.tsx
+++ src/components/about/SceneManager.tsx
@@ -28,7 +28,7 @@ export function SceneManager() {
       duration: isMobile ? 0.8 : 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: false,
-      wheelEventsTarget: document.createElement('div'),
+      eventsTarget: document.createElement('div'),
       syncTouch: true,
       touchMultiplier: isMobile ? 1 : 2,
     })
